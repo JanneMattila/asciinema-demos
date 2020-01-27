86ad8e7833ed80b5a46ef8d1bdbb28e577595b4c
Janne Mattila <janne.mattila@hotmail.fi>
2020-01-27 23:30:25 +0200

    Add ACI launcher script

---
+++ aci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+./demo.sh pwsh aci-create_myaci-rg
+echo -n "# "
+read
+./demo.sh az az-create_acr
+echo -n "# "
+read
+./demo.sh az az-create_aks
+echo -n "# "
+read
+./demo.sh az az-connect_to_aks_with_kubectl
+echo -n "# "
+read
+./demo.sh az az-scale_aks
+echo -n "# "
+read
+./demo.sh az az-delete_aks-rg
